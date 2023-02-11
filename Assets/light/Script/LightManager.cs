52553afad1c8615ad23782c7d3f1d015820d4ef5
Administrator <Administrator@I-01>
2023-02-11 17:32:10 +0900

    Unity
    Unity

---
+++ Assets/light/Script/LightManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    bool[] condition = new bool[3];
+    public GameObject[] lightEffect;
+
+    public void LightSetting(int number)
+    {
+        condition[number] = !condition[number];
+
+        lightEffect[number].SetActive(condition[number]);
+    }
+}
